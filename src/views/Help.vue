aaa46e886a78ccc6a4ce1fa2d74709fc353fc923
gitcjh <1418503647@.qq.com>
2018-03-13 11:25:23 +0800

    backup

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -3,6 +3,23 @@
         <ui-article>
             <h2>说明</h2>
             <p>一个简单的原型制作工具。</p>
+
+            <h2>常用操作</h2>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+
+            <p>暂不支持导入导出已经保存。</p>
+            <!--<ui-article>-->
+            <!--http://img.zcool.cn/community/018fe956400ea732f87512f672b081.jpg-->
+
+            <!--</ui-article>-->
+            <h2>快捷键</h2>
+            <p></p>
         </ui-article>
     </my-page>
 </template>
--- src/views/ImagePage.vue
+++ src/views/ImagePage.vue
@@ -1,31 +1,50 @@
 <template>
-    <my-page title="图片原型工具">
+    <my-page title="图片原型工具" :page="_page">
         <section class="preview-box">
             <ul class="preview-list">
-                <li class="item" v-for="page in pages" @click="selectPage(page)">
-                    <img class="img" :src="page.image"/>
+                <li class="item" v-for="p in pages" @click="setPage(p)"
+                    :title="p.title"
+                    :class="{active: page && p.id === page.id}">
+                    <img class="img" :src="p.image"/>
+                    <div class="footer">
+                        <ui-icon-button class="btn" icon="close" slot="close" @click="removePage(p)" title="删除" />
+                    </div>
+                </li>
+                <li class="item-add" @click="addPage" title="添加页面">
+                    <ui-icon class="icon" value="add" size="48" color="#999" />
                 </li>
             </ul>
-            <a class="add" href="#">添加</a>
+            <ui-raised-button class="add" label="添加页面" @click="addPage" />
         </section>
         <section class="editor-box">
-            <div>编辑模式</div>
+            <div>{{ mode === 'edit' ? '编辑模式' : '预览模式' }}</div>
             <div v-if="page">
-                当前页面：{{ page.id }}
+                <!--当前页面：{{ page.id }}-->
+                <div>当前页面 ID：{{ page.id }}</div>
             </div>
             <div class="btns">
                 <ui-raised-button class="btn" label="返回上一页" @click="back" :disable="!lastPageId" />
-                <ui-raised-button class="btn" label="添加页面" @click="back" />
-                <ui-raised-button class="btn" label="预览" @click="back" />
+                <ui-raised-button class="btn" label="新建" @click="newFile" />
+                <ui-raised-button class="btn" label="保存" @click="saveFile" />
+                <ui-raised-button class="btn" label="预览" @click="preview" />
                 <ui-raised-button class="btn" label="显示所有" @click="showAllPage" />
+                <ui-raised-button class="btn" label="设置" @click="showOption" />
             </div>
             <div>返回上一页</div>
+            <div v-if="!page">
+                <div>请在左侧预览列表中选择页面进行编辑</div>
+            </div>
             <div class="page-editor"
                  @mousemove="onMouseMove"
+                 @mousedown="onMouseDown"
+                 @mouseup="onMouseUp"
                  v-if="page"
                  ref="editor"
                  :style="{width: options.width + 'px', height: options.height + 'px'}">
-                <img :src="page.image"/>
+                <div class="img-wrap">
+                    <img :src="page.image"/>
+                </div>
+                <div class="mask"></div>
                 <div class="links">
                     <div class="link"
                        v-for="link in page.links"
@@ -45,6 +64,13 @@
                         <img src="/static/img/icon/tag.svg">
                     </div>
                 </div>
+                <div class="temp-link"
+                     v-if="tempLink.visible"
+                     :style="{top: tempLink.top + 'px', left: tempLink.left + 'px', width: tempLink.width + 'px', height: tempLink.height + 'px'}"></div>
+            </div>
+            <div class="save-box" v-if="false">
+                <ui-circular-progress :size="24"/>
+                <div class="text">正在保存</div>
             </div>
         </section>
         <div class="link-box">
@@ -54,6 +80,7 @@
                 </div>
                 <div>
                     <ui-text-field v-model="link.to" label="跳转页面ID" />
+                    <ui-raised-button class="btn" label="删除" @click="selectPage" />
                 </div>
                 <div>
                     <ui-text-field v-model="link.left" type="number" label="X" />
@@ -67,18 +94,16 @@
                 <div>
                     <ui-text-field v-model="link.height" type="number" label="高" />
                 </div>
+                <ui-raised-button class="btn" label="删除" @click="removeLink" />
             </div>
         </div>
-        <ui-article>
-            http://img.zcool.cn/community/018fe956400ea732f87512f672b081.jpg
-
-        </ui-article>
         <div class="all-box" v-if="allPageVisible">
-            <ui-appbar title="所有界面"></ui-appbar>
+            <ui-appbar title="所有界面">
+                <ui-icon-button icon="close" slot="left" @click="allPageVisible = false" />
+            </ui-appbar>
             <ul class="all-list">
                 <li class="item page-editor"
                     :id="'all-page-' + p.id"
-                     @mousemove="onMouseMove"
                      ref="editor"
                     v-for="p in pages"
                      :style="{width: options.width + 'px', height: options.height + 'px'}">
@@ -93,6 +118,16 @@
                 </li>
             </ul>
         </div>
+        <ui-drawer right :open="optionVisible" :docked="false" @close="closeOption()">
+            <ui-appbar title="设置">
+                <ui-icon-button icon="close" slot="left" @click="optionVisible = false" />
+            </ui-appbar>
+            <ui-list>
+                <ui-list-item title="Menu Item 1"/>
+                <ui-list-item title="Menu Item 2"/>
+                <ui-list-item title="Menu Item 3"/>
+            </ui-list>
+        </ui-drawer>
     </my-page>
 </template>
 
@@ -100,7 +135,7 @@
     export default {
         data () {
             return {
-                mode: 'editor',
+                mode: 'edit',
                 input: '',
                 result: '1',
                 textFieldLabel: '标签',
@@ -116,6 +151,7 @@
                         image: '/static/img/page_login.png',
                         links: [
                             {
+                                id: '1',
                                 title: '注册',
                                 to: '2',
                                 top: 0,
@@ -124,6 +160,7 @@
                                 height: 100
                             },
                             {
+                                id: '2',
                                 title: '登录',
                                 to: '3',
                                 top: 189,
@@ -147,6 +184,7 @@
                         image: '/static/img/page_register.png',
                         links: [
                             {
+                                id: '3',
                                 title: '登录',
                                 to: '1',
                                 top: 0,
@@ -162,6 +200,7 @@
                         image: '/static/img/page_home.png',
                         links: [
                             {
+                                id: '4',
                                 title: '登录',
                                 to: '1',
                                 top: 459,
@@ -177,6 +216,7 @@
                         image: '/static/img/page_add.png',
                         links: [
                             {
+                                id: '5',
                                 title: '返回',
                                 to: '3',
                                 top: 0,
@@ -190,7 +230,25 @@
                 page: null,
                 link: null,
                 allPageVisible: false,
-                lastPageId: null
+                lastPageId: null,
+                tempLink: {
+                    visible: false,
+                    top: 20,
+                    left: 20,
+                    width: 40,
+                    height: 40
+                },
+                //
+                optionVisible: false,
+                _page: {
+                    menu: [
+                        {
+                            type: 'icon',
+                            icon: 'settings',
+                            click: this.showOption
+                        }
+                    ]
+                }
             }
         },
         mounted() {
@@ -200,14 +258,17 @@
             init() {
                 this.page = this.pages[0]
                 this.rederPage()
+                this.loadFile()
             },
             rederPage() {
             },
-            selectPage(page) {
+            setPage(page) {
                 this.page = page
             },
+            selectPage() {
+            },
             clickLink(link) {
-                if (this.mode === 'editor') {
+                if (this.mode === 'edit') {
                     this.link = link
                 } else {
                     let id = link.to
@@ -220,7 +281,7 @@
                     console.log(page.id)
                     if (page.id === id) {
                         console.log('跳转')
-                        this.lastPageId = thus.page.id
+                        this.lastPageId = this.page.id
                         this.page = page
                     }
                 }
@@ -228,20 +289,138 @@
             click() {
                 alert(1)
             },
+            addPage() {
+                 this.pages.push({
+                     id: new Date().getTime(),
+                     title: '未命名',
+                     image: '/static/img/page_long.jpg',
+                     links: [
+                         {
+                             title: '返回',
+                             to: '3',
+                             top: 0,
+                             left: 0,
+                             width: 40,
+                             height: 40
+                         }
+                     ]
+                 })
+            },
             onMouseMove(e) {
                 console.log(e)
                 let rect = this.$refs.editor.getBoundingClientRect()
                 let x = e.pageX - rect.left
                 let y = e.pageY - rect.top
                 console.log(x, y)
+                if (this.isMouseDown) {
+                    let top = Math.min(this.mouseDownY, y)
+                    let left = Math.min(this.mouseDownX, x)
+                    let width = Math.abs(this.mouseDownX - x)
+                    let height = Math.abs(this.mouseDownY - y)
+                    this.tempLink = {
+                        visible: true,
+                        top: top,
+                        left: left,
+                        width: width,
+                        height: height
+                    }
+                    this.isMouseDrag = true
+                }
+                return false
+            },
+            onMouseDown(e) {
+                console.log(e)
+                let rect = this.$refs.editor.getBoundingClientRect()
+                let x = e.pageX - rect.left
+                let y = e.pageY - rect.top
+                console.log(x, y)
+                this.isMouseDown = true
+                this.mouseDownX = x
+                this.mouseDownY = y
+                return false
+            },
+            onMouseUp(e) {
+                console.log(e)
+                let rect = this.$refs.editor.getBoundingClientRect()
+                let x = e.pageX - rect.left
+                let y = e.pageY - rect.top
+                console.log(x, y)
+
+                this.isMouseDown = false
+                if (this.isMouseDrag) {
+                    this.isMouseDrag = false
+                    this.tempLink.visible = false
+                    let top = Math.min(this.mouseDownY, y)
+                    let left = Math.min(this.mouseDownX, x)
+                    let width = Math.abs(this.mouseDownX - x)
+                    let height = Math.abs(this.mouseDownY - y)
+                    this.page.links.push({
+                        id: new Date().getTime(),
+                        title: '未命名',
+                        to: '',
+                        top: top,
+                        left: left,
+                        width: width,
+                        height: height
+                    })
+                }
+                return false
             },
             back() {
                 if (this.lastPageId) {
-
+                    this.gotoPage(this.lastPageId)
                 }
             },
             showAllPage() {
                 this.allPageVisible = true
+            },
+            showOption() {
+                this.optionVisible = true
+            },
+            removePage(page) {
+                for (let i = 0; i < this.pages.length; i++) {
+                    if (this.pages[i].id === page.id) {
+                        this.pages.splice(i, 1)
+                        break
+                    }
+                }
+                if (this.pages.length) {
+                    this.page = this.pages[0] // TODO
+                } else {
+                    this.page = null
+                }
+            },
+            closeOption() {
+                this.optionVisible = false
+            },
+            removeLink() {
+                for (let i = 0; i < this.page.links.length; i++) {
+                    if (this.page.links[i].id === this.link.id) {
+                        this.page.links.splice(i, 1)
+                        this.link = null
+                        break
+                    }
+                }
+            },
+            preview() {
+                this.mode = 'preview'
+            },
+            newFile() {
+                this.pages = []
+                this.page = null
+            },
+            saveFile() {
+                let saveObj = {
+                    version: '1.0.0',
+                    pages: this.pages
+                }
+                this.$storage.set('tempData', saveObj)
+            },
+            loadFile() {
+                let saveObj = this.$storage.get('tempData')
+                if (saveObj) {
+                    this.pages = saveObj.pages
+                }
             }
         }
     }
